78dc6bb5721c2457bbc6bc5c1ba4ebc6238bcbdf
Gcolin1 <guilhermecolin09@gmail.com>
2024-07-05 19:40:40 -0300

    adicionando splash inicial

--- lib/feature/auth/view/sign_in_screen.dart
+++ lib/feature/auth/view/sign_in_screen.dart
@@ -378,7 +378,8 @@ class _SignInScreenState extends State<SignInScreen> {
                           Get.find<SplashController>().configModel.content?.facebookSocialLogin.toString() == '1' ?
                           SocialLoginWidget(fromPage: widget.fromPage,): const SizedBox(),
                           const SizedBox(height: Dimensions.paddingSizeDefault,),
-                          Row(
+
+                          /*Row(
                             mainAxisAlignment: MainAxisAlignment.center,
                             children: [
                               Text('${'do_not_have_an_account'.tr} ',
@@ -434,7 +435,7 @@ class _SignInScreenState extends State<SignInScreen> {
                               ),)
                               
                             ],
-                          ),
+                          ) */
                           const SizedBox(height: Dimensions.paddingSizeExtraMoreLarge,),
                               
                         ]),
